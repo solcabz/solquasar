28d69d859046dfe6d637f1db8469eb0b2a11d6e5
solcabz <41091153+solcabz@users.noreply.github.com>
2023-07-27 16:00:36 +0800

    set

--- quasar.config.js
+++ quasar.config.js
@@ -156,27 +156,27 @@ module.exports = configure(function (ctx) {
         theme_color: "#027be3",
         icons: [
           {
-            src: "icons/icon-128x128.png",
+            src: "assets/solcabz.png",
             sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "icons/icon-192x192.png",
+            src: "assets/solcabz.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/icon-256x256.png",
+            src: "assets/solcabz.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "icons/icon-384x384.png",
+            src: "assets/solcabz.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "icons/icon-512x512.png",
+            src: "assets/solcabz.png",
             sizes: "512x512",
             type: "image/png",
           },
