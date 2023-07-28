ed72ed60b1619edca3e393159595496f8ad9eb08
solcabz <41091153+solcabz@users.noreply.github.com>
2023-07-28 14:41:03 +0800

    Update settingsPage.vue

--- src/pages/settingsPage.vue
+++ src/pages/settingsPage.vue
@@ -1,3 +1,15 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page>
+    <div class="q-pa-md">
+      <q-layout
+        container
+        style="height: 80px"
+        class="shadow-2 rounded-borders align-item-center"
+      >
+        <p class="q-pa-md">Solocabreza</p>
+      </q-layout>
+    </div>
+  </q-page>
 </template>
+
+sty
